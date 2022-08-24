56672919d15b5ddeb341ddf48e08040839e87ae3
fMurugi <fionamurugi99@gmail.com>
2022-08-24 13:06:30 +0000

    added git configurations

--- installations.sh
+++ installations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.email "fionamurugi99@gmail.com"
+  git config --global user.name "fMurugi"
+
 sudo snap install intellij-idea-community --classic --edge
 
 sudo apt install default-jdk
